3a81bab18c3bacab9bcf29d6a6b0373cd3fdae2b
David Harris <bblldave@gmail.com>
2023-01-15 11:38:53 -0500

    Added CRUD operations for backend

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,29 +1,43 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
 {
-  public class ActivitiesController : BaseApiController
-  {
-    private readonly DataContext _context;
-    public ActivitiesController(DataContext context)
+    public class ActivitiesController : BaseApiController
     {
-      _context = context;
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
+        }
 
-    }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
 
-    [HttpGet] //api/activities
-    public async Task<ActionResult<List<Activity>>> GetActivities()
-    {
-      return await _context.Activities.ToListAsync();
-    }
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivity(Guid id) 
-    {
-      return await _context.Activities.FindAsync(id);
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+        }
     }
-  }
 }
